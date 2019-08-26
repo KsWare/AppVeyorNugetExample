03c6d4e1cdf37d5b28c4f2ca876cc73070a191cf
Kay-Uwe Schreiner <ks@ksware.de>
2019-08-26 10:41:07 +0200

    initial commit

---
+++ src/KsWare.AppVeyorNugetExample/KsWare.AppVeyorNugetExample/ExampleClass.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KsWare.AppVeyorNugetExample
+{
+    public class ExampleClass
+    {
+		public void ExampleMethod() { }
+
+	}
+}
---
+++ src/KsWare.AppVeyorNugetExample/KsWare.AppVeyorNugetExample/Properties/AssemblyInfo.cs
@@ -0,0 +1,17 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("KsWare.AppVeyorNugetExample")]
+[assembly: AssemblyDescription("Example Nuget package")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("KsWare")]
+[assembly: AssemblyProduct("AppVeyorNugetExample")]
+[assembly: AssemblyCopyright("Copyright © 2019 by KsWare. All rights reserved.")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("e5ab4788-e09d-47d3-8e39-8e1a1122411f")]
+
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
